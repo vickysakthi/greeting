a62fc038273021591ce8c1a817d740ae1eac3dc5
vickysakthi <vickysakthi8834@gmail.com>
2021-11-18 12:38:03 +0530

    pass hello

--- hello.test.js
+++ hello.test.js
@@ -2,5 +2,5 @@ var utils  = require('course-utilities');
 var hello = utils.load('./hello.js', 'hello');
 
 test('outputs the correct string', () => {
-  expect(hello()).toBe("Hello World!");
+  expect(hello()).toBe("Hello !");
 });
